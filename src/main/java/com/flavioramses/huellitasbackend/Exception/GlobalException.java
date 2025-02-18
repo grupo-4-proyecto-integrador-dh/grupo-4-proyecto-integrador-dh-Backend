50c9d2a520b64e1d74f09ce5643494f05999bf97
Losi0 <franciscolosievich@gmail.com>
2025-02-17 23:20:50 -0300

    Excepciones agregadas a alojamientoController

--- src/main/java/com/flavioramses/huellitasbackend/Exception/GlobalException.java
+++ src/main/java/com/flavioramses/huellitasbackend/Exception/GlobalException.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalException {
     @ExceptionHandler({ResourceNotFoundException.class})
-
     public ResponseEntity<String> tratamientoRNFE(ResourceNotFoundException rnfe){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("mensaje:" + rnfe.getMessage());
     }
-
+    @ExceptionHandler({BadRequestException.class})
+    public ResponseEntity<String> tratamientoBRE(BadRequestException bre){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("mensaje: " + bre.getMessage());
+    }
 
 }
--- src/main/java/com/flavioramses/huellitasbackend/controller/AlojamientoController.java
+++ src/main/java/com/flavioramses/huellitasbackend/controller/AlojamientoController.java
@@ -1,8 +1,11 @@
 package com.flavioramses.huellitasbackend.controller;
 
+import com.flavioramses.huellitasbackend.Exception.BadRequestException;
+import com.flavioramses.huellitasbackend.Exception.ResourceNotFoundException;
 import com.flavioramses.huellitasbackend.model.Alojamiento;
 import com.flavioramses.huellitasbackend.service.AlojamientoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +20,17 @@ public class AlojamientoController {
     @Autowired
     public AlojamientoService alojamientoService;
 
-    @PostMapping
-    public ResponseEntity<Alojamiento> saveAlojamiento(@RequestBody Alojamiento alojamiento) {
+    @PostMapping("/saveAlojamiento")
+    public ResponseEntity<Alojamiento> saveAlojamiento(@RequestBody Alojamiento alojamiento) throws BadRequestException {
+        /*Alojamiento alojamientoGuardado = alojamientoService.saveAlojamiento(alojamiento);
+        Optional<Alojamiento> alojamientoById = alojamientoService.getAlojamientoById(alojamiento.getId());
+        if(alojamientoById.isPresent()){
+            return ResponseEntity.ok(alojamientoGuardado);
+        }else{
+            throw new BadRequestException("Hubo un error al registrar el alojamiento");
+        }*/
+
+
         return ResponseEntity.status(200).body(alojamientoService.saveAlojamiento(alojamiento));
     }
 
@@ -28,13 +40,23 @@ public class AlojamientoController {
     }
 
     @GetMapping("buscar/{Id}")
-    public ResponseEntity<Optional<Alojamiento>> getAlojamientoById(@PathVariable Long alojamientoId) {
-        return ResponseEntity.status(200).body(alojamientoService.getAlojamientoById(alojamientoId));
+    public ResponseEntity<Optional<Alojamiento>> getAlojamientoById(@PathVariable Long alojamientoId) throws ResourceNotFoundException {
+        Optional<Alojamiento> alojamientoBuscado = alojamientoService.getAlojamientoById(alojamientoId);
+        if(alojamientoBuscado.isPresent()){
+            return ResponseEntity.ok(alojamientoBuscado);
+        }else{
+            throw new ResourceNotFoundException("Alojamiento no encontrado");
+        }
+
     }
 
     @PutMapping
-    public ResponseEntity<Alojamiento> updateAlojamiento(@RequestBody Alojamiento alojamiento) {
-            return ResponseEntity.status(200).body(alojamientoService.updateAlojamiento(alojamiento));
+    public ResponseEntity<Alojamiento> updateAlojamiento(@RequestBody Alojamiento alojamiento) throws BadRequestException {
+          try{
+              return ResponseEntity.ok(alojamientoService.updateAlojamiento(alojamiento));
+          }catch (Exception e){
+              throw new BadRequestException("Ocurrio un error al actualizar el alojamiento");
+          }
     }
 
     @DeleteMapping("/eliminar/{Id}")
